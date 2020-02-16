ccf8870234be03aff4b447ce49956ddf3d32ef88
Yair <yairshop@gmail.com>
2020-02-16 15:47:20 -0500

    cleanup

--- audio-test/simpleaudio.py
+++
@@ -1,6 +0,0 @@
-import simpleaudio
-
-filename = 'output.wav'
-wave_obj = simpleaudio.WaveObject.from_wave_file(filename)
-play_obj = wave_obj.play()
-play_obj.wait_done()  # Wait until sound has finished playing
