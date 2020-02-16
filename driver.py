c40c1af0929fc2c250b3f10631d4a8566888440a
Yair <yairshop@gmail.com>
2020-02-16 08:20:00 -0500

    update args and accept taamim from command line

--- driver.py
+++ driver.py
@@ -1,26 +1,42 @@
 import argparse
 from itertools import groupby
+import sys
+from aubio import midi2note
 from analyze import *
 from plotter import *
 from taamim_torah import *
 from utils import *
-from aubio import midi2note
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--trop', '-t', type=str, required=True, nargs="+")
+parser.add_argument('--taam', '-t', type=str, required=True, nargs="+", help="""
+Sequence of t'amim to be played. Options are:
+    ['munach-zarka', 'zarka', 'munach-segol',
+    'segol', 'munach-munach-rvii', 'munach-rvii', 'rvii',
+    'mapakh', 'pashta', 'munach-katon', 'zakef-katon',
+    'mercha', 'tipcha', 'munach-etnachta', 'etnachta',
+    'pazer', 'tlisha-ktana', 'tlisha-gdola', 'kadma',
+    'vazla', 'azla-geresh', 'gershaim', 'darga', 'tvir', 
+    'yetiv', 'shalshelet', 'sof-pasuk'].
+Note that the symbols for different munachim include
+the t'amim that follow them for the sake of differentiation,
+but the actual taam is still just a munach. For example,
+'munach-munach-rvii' is the second munach preceding a rvii.
+""")
 
 args = parser.parse_args()
 
+given_taamim = args.taam
+for taam in given_taamim:
+    if taam not in trop:
+        print("Taam '%s' not found" % taam)
+        sys.exit(1)
+
 given = extract_notes_from_file(0, 'test2.m4a')
 
-offset = given[0] - trop_notes['munach-munach-rvii'][0]
+offset = given[0] - trop_notes[given_taamim[0]][0]
 given_transpose = [i - offset for i in given]
 
-expected_notes, expected_timing, pronunc = get_notes(['munach-munach-rvii', 'munach-rvii', 'rvii'])
-
-print(expected_notes)
-print(expected_timing)
-print(pronunc)
+expected_notes, expected_timing, pronunc = get_notes(given_taamim)
 
 plot_taam(expected_notes, expected_timing, given_transpose, midi2note(given[0]), pronunc)
 
