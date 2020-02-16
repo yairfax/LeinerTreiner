0d7ddd707782f5aef4cb5b79c72480d69161eed9
Joshua Bloch <supercavitation@wireless-10-104-37-144.umd.edu>
2020-02-16 12:57:10 -0500

    I'm hoping this works because Aaron isn't here and I'm hopeless with git without him

--- web scraping practice2.py
+++ web scraping practice2.py
@@ -4,8 +4,6 @@ from bs4 import NavigableString
 import pandas as pd
 from time import time
 
-sounds1=['w','a','y','ə','d','b','ē','r','‘','ă','l','ê','ḵ','e','m','i','t','ô','h','ā','š','q','b','î','ō','û','t','n','z','p','k','ṣ','ĕ','’']
-sounds2=['V','AA','Y','IH','D','B','AY','R','','AA','L','EY','/H','EH','M','IY','T','OH','/H','AA','SH','K','V','IY','OH','UX','T','N','Z','F','K','TZ','EH','']
 sefer=input('Which sefer would you like to see? ')
 perek=input('Which perek would you like to see? ')       
 
@@ -83,3 +81,82 @@ tropout=" ".join(tropout)
 print(pasukoutheb)
 print(pasukout)
 print(tropout)
+
+out=[]
+taamim={
+    'ZAR2':'zarka',
+    'SGL1':'SEGOL',
+    'RVI2':'rivii',
+    'MHP4':'mapakh',
+    'PSH2':'pashta',
+    'MER4':'mercha',
+    'TIP1':'tipcha',
+    'ATN0':'etnachta',
+    'PAZ3':'pazer',
+    'TLQ4':'tlisha-ktana',
+    'TLG3':'tlisha-gdola',
+    'QAD4':'kadma',
+    'DAR4':'darga',
+    'TVR2':'tvir',
+    'YTV2':'yetiv',
+    'SHA1':'shalshelet',
+    'SLQ0':'sof-pasuk',
+    'GRM3':'gershaim',
+    'ZQQ1':'zakef-katon',
+    'ZQG1':'zakef-gadol',
+    'LGM3':'munach-munach-rvii'
+}
+printer=False
+found=False
+for i in range(len(notes)):
+    if isinstance(notes[i],int) and notes[i]!=pasuk:
+        printer=False
+    if printer==True:
+        if notes[i] in taamim:
+            out.append(taamim[notes[i]])
+        elif notes[i]=='GER3':
+            if i==0:
+                out.append('azla-geresh')
+            else:
+                j=i-1
+                while j>0 and notes[j]=='MUN4' or notes[j]=='MQF5':
+                    j-=1
+                if notes[j]=='QAD4':
+                    out.append('vazla')
+                else:
+                    out.append('azla-geresh')
+
+        elif notes[i]=='MUN4':
+            j=i
+            while notes[j+1]=='MQF5':
+                j+=1
+            if notes[j+1]=='ZAR2':
+                out.append('munach-zarka')
+            elif notes[j+1]=='SGL1':
+                out.append('munach-segol')
+            elif notes[j+1]=='RVI2':
+                out.append('munach-rvii')
+                print(words[i])
+            elif notes[j+1]=='ATN0':
+                out.append('munach-etnachta')
+            elif notes[j+1]=='MUN4':
+                while notes[j+1]=='MUN4':
+                    while notes[j+1]=='MQF5':
+                        j+=1
+                    j+=1
+                if notes[j+1]=='ZAR2':
+                    out.append('munach-zarka')
+                elif notes[j+1]=='SGL1':
+                    out.append('munach-segol')
+                elif notes[j+1]=='ATN0':
+                    out.append('munach-etnachta')
+                else:
+                    out.append('munach')
+            else:
+                out.append('munach')
+    if isinstance(notes[i],int) and notes[i]==pasuk and found==False:
+        printer=True
+        found=True
+
+out=" ".join(out)
+print(out)
